eef48afc3d87d5d2f71ff344d89dfab5824fc448
matthew <matthew@secrettune.io>
2022-07-14 11:59:36 +0800

    v2 docs

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -1,63 +1,63 @@
-import React from 'react';
-import Link from '@docusaurus/Link';
-import clsx from 'clsx';
-import styles from './HomepageFeatures.module.css';
+import React from "react";
+import Link from "@docusaurus/Link";
+import clsx from "clsx";
+import styles from "./HomepageFeatures.module.css";
 
 const FeatureList = [
   {
-    title: 'Learn about us',
-    Svg: require('../../static/img/logo.svg').default,
-    description: (
-      <>
-        All you need to know to get started on your Pendle journey
-      </>
-    ),
-    url: '/docs/information/intro',
+    title: "Introduction",
+    Svg: require("../../static/img/intro.svg").default,
+    url: "/docs/reference/intro"
   },
   {
-    title: 'Build on top of Pendle',
-    Svg: require('../../static/img/database.svg').default,
-    description: (
-      <>
-        Build DApps on top of the Pendle ecosystem
-      </>
-    ),
-    url: '/docs/guides/intro',
+    title: "Pendle - Simple",
+    Svg: require("../../static/img/intro.svg").default,
+    url: "/docs/reference/intro"
   },
   {
-    title: 'Contract reference',
-    Svg: require('../../static/img/contracts.svg').default,
-    description: (
-      <>
-        Detailed documentation on Pendle's contracts
-      </>
-    ),
-    url: '/docs/reference/intro',
+    title: "Pendle - Pro",
+    Svg: require("../../static/img/intro.svg").default,
+    url: "/docs/reference/intro"
   },
   {
-    title: 'List of addresses',
-    Svg: require('../../static/img/addresses.svg').default,
-    description: (
-      <>
-        The necessary Pendle contract addresses across the different chains
-      </>
-    ),
-    url: '/docs/addresses/avalanche',
+    title: "Governance",
+    Svg: require("../../static/img/intro.svg").default,
+    url: "/docs/reference/intro"
+  },
+  {
+    title: "Developers",
+    Svg: require("../../static/img/intro.svg").default,
+    url: "/docs/reference/intro"
+  },
+  {
+    title: "Contracts",
+    Svg: require("../../static/img/intro.svg").default,
+    url: "/docs/reference/intro"
+  },
+  {
+    title: "Purchase Assets at a Discount",
+    Svg: require("../../static/img/intro.svg").default,
+    url: "/docs/reference/intro"
+  },
+  {
+    title: "Farm",
+    Svg: require("../../static/img/intro.svg").default,
+    url: "/docs/reference/intro"
+  },
+  {
+    title: "Yield Trading",
+    Svg: require("../../static/img/intro.svg").default,
+    url: "/docs/reference/intro"
   },
 ];
 
-function Feature({Svg, title, description, url}) {
+function FeatureCard({ url, title, Svg }) {
   return (
-    <Link
-    className="button col col--4 item shadow--md"
-    to={url}>
-      <div>
-        <div className="text--center">
-          <Svg className={styles.featureSvg} alt={title} />
-        </div>
-        <div className={`text--left padding-left--xl ${styles.featureText}`}>
+    <Link to={url}>
+      <div className={styles.featureCard}>
+        <Svg role="img" className={styles.image} />
+        <div className={styles.featureTitle}>
           <h3>{title}</h3>
-          <p>{description}</p>
         </div>
       </div>
     </Link>
@@ -66,14 +66,13 @@ function Feature({Svg, title, description, url}) {
 
 export default function HomepageFeatures() {
   return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className={`row ${styles.featureRow}`}>
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
+    <div className={styles.root}>
+      <h2 className={styles.header}>Getting Started</h2>
+      <div className={styles.featureContainer}>
+        {FeatureList.map((feature) => (
+          <FeatureCard {...feature} />
+        ))}
       </div>
-    </section>
+    </div>
   );
 }
