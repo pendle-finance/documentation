f7f459aa2789420c92743753d6a4379d3aacac83
NikolaiKryshnev <63440682+NikolaiKryshnev@users.noreply.github.com>
2025-03-05 10:47:12 +0300

    Update index.js

--- src/theme/Footer/index.js
+++ src/theme/Footer/index.js
@@ -26,7 +26,7 @@ function Footer() {
             Discord
           </a>
           <a href="https://x.com/pendle_fi/" target="_blank">
-            X (ex-Twitter)
+            X
           </a>
           <a href="https://t.me/pendlefinance/" target="_blank">
             Telegram
