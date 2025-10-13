215a7ffb6002176be87c826acaa17047eda1a492
ayobuenavista <ayobuenavista@users.noreply.github.com>
2025-10-13 23:08:32 +0000

    Built artifacts of c174a58d [ci skip]

---
+++ cn/assets/js/223a3177.010dbcdd.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkpendle_documentation=self.webpackChunkpendle_documentation||[]).push([[241],{762:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"pendle-academy"}')}}]);
