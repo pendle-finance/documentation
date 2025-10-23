b8b16083d74a0f1cdfbd1877e4b0317e6c0e7694
Minh Hung Tran <hung@pendle.finance>
2025-10-23 14:05:08 +0700

    feat: Use local search as replacement for algolia

--- docusaurus.config.mjs
+++ docusaurus.config.mjs
@@ -42,6 +42,30 @@ const config = {
     ],
   ],
 
+  themes: [
+    // ... Your other themes.
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+      ({
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+
+        // For Docs using Chinese, it is recomended to set:
+        language: ["en", "zh"],
+        docsRouteBasePath: ["/pendle-v2", "/boros-dev"],
+
+        // Customize the keyboard shortcut to focus search bar (default is "mod+k"):
+        // searchBarShortcutKeymap: "s", // Use 'S' key
+        // searchBarShortcutKeymap: "ctrl+shift+f", // Use Ctrl+Shift+F
+
+        // If you're using `noIndex: true`, set `forceIgnoreNoIndex` to enable local index:
+        // forceIgnoreNoIndex: true,
+      }),
+    ],
+  ],
+
   plugins: [
     [
       '@docusaurus/plugin-client-redirects',
